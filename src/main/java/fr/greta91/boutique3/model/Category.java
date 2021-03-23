98a1eeff3c91a6b0964ef09a4b9a58087f9dcb28
k59_9 <k59_9@LAPTOP-P4N3OS44>
2021-03-23 14:36:44 +0100

    supplémentaire

--- src/main/java/fr/greta91/boutique3/model/Category.java
+++ src/main/java/fr/greta91/boutique3/model/Category.java
@@ -12,10 +12,6 @@ import javax.persistence.Table;
 @Entity
 @Table(name="categorie")
 public class Category implements Serializable{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_categorie")
