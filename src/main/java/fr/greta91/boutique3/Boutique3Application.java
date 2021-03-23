f77bb2239f369ca237d8883e37998088b363e05d
k59_9 <k59_9@LAPTOP-P4N3OS44>
2021-03-23 10:10:30 +0100

    creation de projet https://trello.com/c/PxnYZzAL

---
+++ src/main/java/fr/greta91/boutique3/Boutique3Application.java
@@ -0,0 +1,13 @@
+package fr.greta91.boutique3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boutique3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boutique3Application.class, args);
+	}
+
+}
---
+++ src/test/java/fr/greta91/boutique3/Boutique3ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.greta91.boutique3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boutique3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
