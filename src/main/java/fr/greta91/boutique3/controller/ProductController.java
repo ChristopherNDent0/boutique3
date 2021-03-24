6543a3c25373e55239747ec93df105c69322a73e
k59_9 <k59_9@LAPTOP-P4N3OS44>
2021-03-24 17:02:43 +0100

    les méthodes delete save edit

--- src/main/java/fr/greta91/boutique3/controller/ProductController.java
+++ src/main/java/fr/greta91/boutique3/controller/ProductController.java
@@ -5,8 +5,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import fr.greta91.boutique3.model.Product;
@@ -23,11 +27,7 @@ public class ProductController {
 	@GetMapping("")
 	public List<Product> getProducts() {
 		List<Product> list = productRepo.findAll();
-//		list.add("produit1");
-//		list.add("produit2");
-//		list.add("produit3");
-//		list.add("produit4");
-//		list.add("produit5");
+
 		return list;
 	}
 
@@ -40,6 +40,45 @@ public class ProductController {
 			return ResponseEntity.notFound().build();
 		}
 
+	}
+	@PostMapping("/edit")
+	public ResponseEntity<Product> createProduct(@RequestBody Product product){
+		try {
+			Product res = productRepo.save(product);
+			return ResponseEntity.ok(res);
+		}
+		catch(Exception ex) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@DeleteMapping("/edit/{id}")  // A revoir url
+	public ResponseEntity<Product> deleteProduct(@PathVariable int id) {
+		try {
+			Product product = productRepo.findById(id).get();
+			if(null != product) {
+				productRepo.delete(product);
+				return ResponseEntity.ok(product);
+			}
+			else {
+				return ResponseEntity.notFound().build();
+			}
+		}
+		catch(Exception ex) {
+			System.out.println("In ProductController.deleteProduct(), exception : " + ex); // not sure if needed
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@PutMapping("/edit/{id}") // A revoir url
+	public ResponseEntity<Product> modifyProduct(@PathVariable int id, @RequestBody Product product) {
+		try {
+			Product res = productRepo.save(product);
+			return ResponseEntity.ok(res);
+		}
+		catch(Exception ex) {
+			return ResponseEntity.notFound().build();
+		}
 	}
 
 }
